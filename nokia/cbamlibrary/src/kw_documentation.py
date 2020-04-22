2fba85def713dfad0d884e46666cdb1cd026e8b1
Pasi Saikkonen <pasi.saikkonen@eficode.com>
2020-04-22 15:59:48 +0300

    Add missing keyword documentations

--- nokia/cbamlibrary/src/kw_documentation.py
+++ nokia/cbamlibrary/src/kw_documentation.py
@@ -52,9 +52,98 @@ _Using .env file and requests configuration kwargs (Disable SSL Cert verificatio
 create_vnf = """Creates a new VNF using given VNF descriptor. Returns a JSON description of the created VNF.
 
 *Arguments:*\n
-``vnfd_id`` vnfdId of an onboarded VNF descriptor. List of available VNFDs can be fetched using keyword `Get VNFDs`.\n
+``vnfd_id`` vnfdId of an onboarded VNF descriptor. List of all available VNFDs can be fetched using keyword `Get VNFDs`.
+VNFD ID can also be taken from the return value of `Onboard VNFD` keyword.\n
 ``name`` Name of the created VNF
+
+*Example return value:*\n
+| {
+|     "vnfInstanceName": "EXAMPLE-VNF",
+|     "vnfProductName": "Example VNF",
+|     "vnfSoftwareVersion": "1.0",
+|     "instantiationState": "NOT_INSTANTIATED",
+|     "vimConnectionInfo": [],
+|     "vnfProvider": "Example",
+|     "vnfdId": "example_vnfd",
+|     "extensions": {
+|         "security_group": [
+|             "example-group"
+|         ],
+|         "up_core_gateway": "127.0.0.1",
+|         "sig_core_gateway": "127.0.0.1",
+|         "oam_gateway": "127.0.0.1",
+|         "ue_sim_gateway": "127.0.0.1"
+|     },
+|     "vnfdVersion": "1.0",
+|     "vnfConfigurableProperties": {
+|         "operation_triggers": {}
+|     },
+|     "vnfPkgId": "example_vnfd",
+|     "id": "CBAM-1234abcde56789fghijklmn",
+|     "metadata": {}
+| }
 
 *Example:*\n
 | Create VNF | example-vnfd-id | Example VNF |
+"""
+
+
+delete_vnf = """Deletes VNF by id.
+
+*Arguments:*\n
+``vnf_id`` ID of the VNF that will be deleted
+
+*Example:*\n
+| Delete VNF | CBAM-1234abcde56789fghijklmn |
+"""
+
+
+disable_insecure_request_warning = """Disables urllib3.exceptions.InsecureRequestWarning which is printed on each 
+request when SSL Cert verification is disabled.
+"""
+
+
+get_vnfs = """Prints and returns a list of all VNFs.
+
+*Example:*\n
+_Log names of all VNF instances_
+| ${VNFs} | Get VNFs |
+| FOR | ${VNF} | IN | @{VNFs} |
+| | LOG | ${VNF}[vnfInstanceName] |
+| END |
+"""
+
+
+get_vnfds = """Prints and returns a list of all VNFDs.
+
+*Example:*\n
+_Log names of all VNFDs_
+| ${VNFDs} | Get VNFDs |
+| FOR | ${VNFD} | IN | @{VNFDs} |
+| | LOG | ${VNFD}[name] |
+| END |
+"""
+
+
+onboard_vnfd = """Uploads the VNF template package to the CBAM VNF catalog. Returns a JSON description of the VNFD.
+
+*Arguments:*\n
+``vnfd`` Path to the vnfd zip file
+
+*Example return value:*\n
+| {
+|     "id": "1",
+|     "name": "example_vnfd",
+|     "version": "1.0",
+|     "references": [],
+|     "vnfdId": "example_vnfd",
+|     "vimType": "openstack",
+|     "provider": "Example",
+|     "productName": "Example VNF",
+|     "swVersion": "1.0"
+| }
+
+*Example:*\n
+| ${vnfd} | Onboard VNFD | /Users/Robot/Documents/project/vnfd.zip |
+| Log | ${vnfd}[vnfdId] | # Log the VNFD ID. This could be given as an argument for `Create VNF` keyword. |
 """
