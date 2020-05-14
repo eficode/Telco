042f4568d8417550cade75260ed43c237ad48685
Joni Lampio <joni.lampio@eficode.com>
2020-05-14 09:49:13 +0300

    Pass connection to catalog so that catalog class can do API requests. This allows different implementations of keywords for different catalog versions.

--- nokia/cbamlibrary/src/CBAMLibrary.py
+++ nokia/cbamlibrary/src/CBAMLibrary.py
@@ -28,7 +28,7 @@ class CBAMLibrary:
         client_id = client_id or os.getenv("CLIENT_ID")
         client_secret = client_secret or os.getenv("CLIENT_SECRET")
         self.connection = Connection(host, client_id, client_secret, **kwargs)
-        self.catalog = Catalog._get_version(catalog_version)
+        self.catalog = Catalog(self.connection)._get_version(catalog_version)
 
     def create_vnf(self, vnfd_id, name, description=None):
         payload = {
@@ -75,9 +75,7 @@ class CBAMLibrary:
         response = self.connection.patch(f"/vnflcm/v1/vnf_instances/{vnf_id}", json=modifications)
 
     def onboard_vnfd(self, vnfd):
-        response = self.connection.post(self.catalog.endpoint, files= {"content": open(vnfd, "rb")})
-        return response.json()
-
+        return self.catalog.onboard_vnfd(vnfd)
 
 class Connection:
     def __init__(self, host, client_id, client_secret, **kwargs):
@@ -124,6 +122,9 @@ class Connection:
     def post(self, path, **kwargs):
         return self.request("post", path, **kwargs)
 
+    def put(self, path, **kwargs):
+        return self.request("put", path, **kwargs)
+
     def delete(self, path, **kwargs):
         return self.request("delete", path, **kwargs)
 
@@ -134,19 +135,40 @@ class Connection:
 class CatalogSOL005:
     endpoint = "/vnfpkgm/v1/vnf_packages"
 
+    def __init__(self, connection):
+        self.connection = connection
+
+    def onboard_vnfd(self, vnfd):
+        response = self.create_vnf_package_resource()
+        self.upload_vnf_package_content(response['id'], vnfd)
+        return response
+
+    def create_vnf_package_resource(self):
+        response = self.connection.post(self.endpoint)
+        return response.json()
+
+    def upload_vnf_package_content(self, vnfPkgId, vnfd):
+        self.connection.put(self.endpoint + f"/{vnfPkgId}/package_content", headers = {'Content-Type': 'application/zip'}, data = open(vnfd, "rb"))
 
 class Catalog18:
     endpoint = "/api/catalog/adapter/vnfpackages"
 
+    def __init__(self, connection):
+        self.connection = connection
+
+    def onboard_vnfd(self, vnfd):
+        response = self.connection.post(self.endpoint, files= {"content": open(vnfd, "rb")})
+        return response.json()
 
 class Catalog:
-    SOL005 = CatalogSOL005()
-    v18 = Catalog18()
 
-    @staticmethod
-    def _get_version(version):
+    def __init__(self, connection):
+        self.SOL005 = CatalogSOL005(connection)
+        self.v18 = Catalog18(connection)
+
+    def _get_version(self, version):
         try:
-            return getattr(Catalog, version)
+            return getattr(self, version)
         except AttributeError:
-            existing_versions = list(filter(lambda x: not x.startswith("_"), Catalog.__dict__.keys()))
+            existing_versions = list(filter(lambda x: not x.startswith("_"), self.__dict__.keys()))
             raise ValueError(f"No such catalog version: '{version}'. Supported versions are: {existing_versions}") from None
